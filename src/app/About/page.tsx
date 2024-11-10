bae78b891fd2c4bd1c1a9d8ed61ca65c09db9328
Code-Master-42 <rarain904@gmail.com>
2024-11-10 12:40:36 +0500

    first commit

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -3,11 +3,12 @@ import "@/style/about.css"
 import Image from 'next/image'
 const About = () => {
   return (
-    <div className='container'>
+    <div className='container '>
+      <div>
           <h2 className='hho'>About Me</h2>
 
       
-    <div className='kk'> <p>Hi I'am Rizwan Arain<br/>I am a passionate and dedicated Full Stack Developer with a strong focus on front-end technologies. Over the years, I've honed my skills in <span className='yy'>HTML</span> <span className='yy'>Css</span>, <span className='yy'>JavaScript</span>, and <span className='yy'>TypeScript</span>. I've expanded my expertise to modern frameworks such as <span className='yy'>Next.js</span>,<br/> always striving to stay up-to-date with the latest technologies.</p>
+    <div className='kk'> <p>Hi I'am Rizwan Arain<br/>I am a passionate and dedicated Full Stack Developer with a strong focus on front-end technologies. Over the years, I've honed my skills in <span className='yy'>HTML</span> <span className='yy'>Css</span>, <span className='yy'>JavaScript</span>, and <span className='yy'>TypeScript</span>. I've expanded my expertise to modern frameworks such as <span className='yy'>Next.js</span>,<br/> always striving to stay up-to-date with the latest technologies.</p></div>
     <Image src={"/kaka11.jpg"} alt='ptofile' width={200} height={200} className='dd'/>
   </div></div>
   )
